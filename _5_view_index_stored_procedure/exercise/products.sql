ef55d19c6c9222be2a905042c2018fad4cfe1d1e
Kien1204 <nguyendtrungkien1204@gmail.com>
2022-05-18 11:27:52 +0700

    update

---
+++ _5_view_index_stored_procedure/exercise/products.sql
@@ -0,0 +1,10 @@
+create database product ;
+use product ; 
+create table product(
+id  int,
+product_code int,
+product_price int,
+product_amount int,
+product_description varchar(50),
+product_status varchar(50)
+);
